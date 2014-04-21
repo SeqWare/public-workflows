c0a1fbd256b275ea72c81d8b1c810ae20e3eecaa
Brian O'Connor <boconnor@my-mac-pro.local>
2014-04-21 08:15:28 -0400

    Update

--- decider-bwa-pancancer/query_gnos_repos.sh
+++ decider-bwa-pancancer/query_gnos_repos.sh
@@ -3,8 +3,7 @@
 # queries each gnos repo
 
 for i in gtrepo-bsc gtrepo-dkfz gtrepo-osdc gtrepo-etri gtrepo-ebi; do 
-echo $i; 
-  perl workflow_decider.pl --gnos-url https://$i.annailabs.com --report $i.log --ignore-lane-count --upload-results --test
+  perl workflow_decider.pl --gnos-url https://$i.annailabs.com --report $i.log --ignore-lane-count --upload-results --test &> /dev/null
   echo "SITE: $i";
   cat $i.log | grep 'ALIGNMENT:' | sort | uniq -c
 done;
