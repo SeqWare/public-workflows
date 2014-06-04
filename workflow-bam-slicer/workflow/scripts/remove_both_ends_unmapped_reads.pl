edf31860cccbe858559f2cf64e16f67adea14bbc
Junjun Zhang <junjun.zhang@oicr.on.ca>
2014-06-04 11:59:07 -0400

    bug fix in the new perl script

--- workflow-bam-slicer/workflow/scripts/remove_both_ends_unmapped_reads.pl
+++ workflow-bam-slicer/workflow/scripts/remove_both_ends_unmapped_reads.pl
@@ -11,6 +11,6 @@ while(<STDIN>){
     my @F = split /\t/, $_, -1;
 
     # don't output if both ends are unmapped. It may be better to check the FLAG field, but let's do the simple way first
-    print $_ unless ($F[2] eq "*" && $F[6] eq "*");
+    print "$_\n" unless ($F[2] eq "*" && $F[6] eq "*");
 
 }
