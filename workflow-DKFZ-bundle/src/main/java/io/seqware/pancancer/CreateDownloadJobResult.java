5a07b4e161e2672c3ce4d82e5cda09bb44ad2e51
dankwart-de <mcheinold@gmail.com>
2014-09-18 09:32:32 +0000

    A lot of changes and bugfixes. GNOS download now only starts if no checkpoint file exists (checkpoint.txt file within the download dir). All three Roddy pipelines are now started. By default a premade delly file is used for aceseq.

--- workflow-DKFZ-bundle/src/main/java/io/seqware/pancancer/CreateDownloadJobResult.java
+++ workflow-DKFZ-bundle/src/main/java/io/seqware/pancancer/CreateDownloadJobResult.java
@@ -1,14 +1,15 @@
 package io.seqware.pancancer;
 	
+import java.io.*;
 import net.sourceforge.seqware.pipeline.workflowV2.*;
 import net.sourceforge.seqware.pipeline.workflowV2.model.*;
 
 class CreateDownloadJobResult {
 	public final Job job;
 	public final String elementID;
-	public String outputDirectory;
+	public File outputDirectory;
 	
-	public CreateDownloadJobResult(Job job, String elementID, String outputDirectory) {
+	public CreateDownloadJobResult(Job job, String elementID, File outputDirectory) {
 		this.job = job;
 		this.elementID = elementID;
 		this.outputDirectory = outputDirectory;
