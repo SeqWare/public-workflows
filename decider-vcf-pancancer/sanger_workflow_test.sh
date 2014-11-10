f5700f44b1f5eb56b10d1eefb028c0742e026d65
Sheldon McKay <sheldon.mckay@gmail.com>
2014-11-10 14:39:48 +0000

    test scripts

---
+++ decider-vcf-pancancer/sanger_workflow_test.sh
@@ -0,0 +1,9 @@
+perl bin/sanger_workflow_decider.pl \
+--schedule-force-run \
+--seqware-clusters cluster.json \
+--workflow-version 2.6.0 \
+--working-dir small \
+--gnos-url  https://gtrepo-etri.annailabs.com  \
+--decider-config conf/decider.ini \
+--use-cached-analysis \
+--schedule-whitelist-donor donors_I_want.txt \
