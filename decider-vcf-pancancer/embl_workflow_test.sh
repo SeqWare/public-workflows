4dbfed60c67d1829f02a4a43f990c7014c8ac53f
Sheldon McKay <sheldon.mckay@gmail.com>
2014-12-04 14:56:45 +0000

    cleanup

--- decider-vcf-pancancer/embl_workflow_test.sh
+++
@@ -1,9 +0,0 @@
-perl bin/embl_workflow_decider.pl \
---schedule-force-run \
---seqware-clusters cluster.json \
---workflow-version 2.6.0 \
---working-dir small \
---gnos-url  https://gtrepo-etri.annailabs.com  \
---decider-config conf/decider.ini \
---use-cached-analysis \
---schedule-whitelist-donor donors_I_want.txt \
