3419930e245f1a9a2cffd59aff2173e352d8f08e
Brian O'Connor <Brian.OConnor@oicr.on.ca>
2015-03-10 17:40:11 +0000

    Redirect sample workflow ini

--- DEWrapperWorkflow/launchWorkflowDev.sh
+++ DEWrapperWorkflow/launchWorkflowDev.sh
@@ -2,5 +2,5 @@
 set -o errexit
 set -o pipefail
 
-wget https://raw.githubusercontent.com/SeqWare/public-workflows/feature/workflow-DKFZ-EMBL-wrap-workflow/DEWrapperWorkflow/sample.ini
-seqware bundle launch --dir /workflows/Workflow_Bundle_DEWrapperWorkflow_1.0-SNAPSHOT_SeqWare_1.1.0-rc.1 --ini sample.ini --engine whitestar --no-metadata
+wget https://raw.githubusercontent.com/SeqWare/public-workflows/feature/workflow-DKFZ-EMBL-wrap-workflow/DEWrapperWorkflow/workflow/config/DEWrapperWorkflow.ini
+seqware bundle launch --dir /workflows/Workflow_Bundle_DEWrapperWorkflow_1.0-SNAPSHOT_SeqWare_1.1.0-rc.1 --ini DEWrapperWorkflow.ini --engine whitestar --no-metadata
